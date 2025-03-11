c4ef30cd0fe1c91643e6a8230cf45b796a22cce7
dao phuong <phuongdao852001@gmail.com>
2025-03-12 01:51:05 +0700

    Sports Center

---
+++ src/data/sportsCenter.js
@@ -0,0 +1,105 @@
+const sportsCenters = [
+    {
+        "name": "Khu thể thao ABC",
+        "phone_number": "0123456789",
+        "address": "123 Đường XYZ, Quận 1",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.700, 10.780]
+        },
+        "description": "Cụm sân với đầy đủ tiện nghi."
+    },
+    {
+        "name": "Khu thể thao DEF",
+        "phone_number": "0987654321",
+        "address": "456 Đường UVW, Quận 2",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.800, 10.790]
+        },
+        "description": "Khu thể thao hiện đại với nhiều sân chơi."
+    },
+    {
+        "name": "Khu thể thao GHI",
+        "phone_number": "0112233445",
+        "address": "789 Đường RST, Quận 3",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.900, 10.800]
+        },
+        "description": "Khu thể thao với nhiều tiện ích và dịch vụ."
+    },
+    {
+        "name": "Khu thể thao JKL",
+        "phone_number": "0223344556",
+        "address": "101 Đường ABC, Quận 4",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.710, 10.770]
+        },
+        "description": "Khu thể thao với sân bóng đá và bóng rổ."
+    },
+    {
+        "name": "Khu thể thao MNO",
+        "phone_number": "0334455667",
+        "address": "202 Đường DEF, Quận 5",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.720, 10.760]
+        },
+        "description": "Khu thể thao với sân tennis và bể bơi."
+    },
+    {
+        "name": "Khu thể thao PQR",
+        "phone_number": "0445566778",
+        "address": "303 Đường GHI, Quận 6",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.730, 10.750]
+        },
+        "description": "Khu thể thao với sân cầu lông và phòng gym."
+    },
+    {
+        "name": "Khu thể thao STU",
+        "phone_number": "0556677889",
+        "address": "404 Đường JKL, Quận 7",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.740, 10.740]
+        },
+        "description": "Khu thể thao với sân bóng chuyền và bóng bàn."
+    },
+    {
+        "name": "Khu thể thao VWX",
+        "phone_number": "0667788990",
+        "address": "505 Đường MNO, Quận 8",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.750, 10.730]
+        },
+        "description": "Khu thể thao với sân bóng đá mini và sân golf."
+    },
+    {
+        "name": "Khu thể thao YZ",
+        "phone_number": "0778899001",
+        "address": "606 Đường PQR, Quận 9",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.760, 10.720]
+        },
+        "description": "Khu thể thao với sân bóng rổ và sân tennis."
+    },
+    {
+        "name": "Khu thể thao ABCD",
+        "phone_number": "0889900112",
+        "address": "707 Đường STU, Quận 10",
+        "location": {
+            "type": "Point",
+            "coordinates": [106.770, 10.710]
+        },
+        "description": "Khu thể thao với sân cầu lông và bể bơi."
+    }
+
+];
+
+export default sportsCenters;
