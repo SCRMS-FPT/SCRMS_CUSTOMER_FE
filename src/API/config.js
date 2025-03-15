c9f56a5e7095f67c90f54025667c29875d58ebc9
Lê Minh Thắng <107106191+lethangd@users.noreply.github.com>
2025-03-15 21:12:34 +0700

    add all api function CourtApi.ts

--- src/API/config.js
+++ src/API/config.js
@@ -4,3 +4,4 @@ export const API_MATCHING_URL = "https://localhost:7092";
 export const API_REVIEW_URL = "https://localhost:7052";
 export const API_CHAT_URL = "https://localhost:7219";
 export const API_PAYMENT_URL = "https://localhost:7107";
+export const API_COURT_URL = "https://localhost:7158";
