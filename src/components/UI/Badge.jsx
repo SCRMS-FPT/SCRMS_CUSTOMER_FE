e8c76c6cd3fcc879d4069810df12944b584244e9
Lê Minh Thắng <107106191+lethangd@users.noreply.github.com>
2025-03-26 12:10:55 +0700

    refactor: remove unused UI components and clean up codebase

--- src/components/UI/Badge.jsx
+++
@@ -1,9 +0,0 @@
-// /src/components/UI/Badge.jsx
-import React from "react";
-import { Badge } from "antd";
-
-const CustomBadge = (props) => {
-  return <Badge {...props} />;
-};
-
-export default CustomBadge;
--- src/components/UI/index.js
+++
@@ -1,5 +0,0 @@
-// /src/components/UI/index.js
-export { default as BaseButton } from "./BaseButton";
-export { default as BaseModal } from "./BaseModal";
-export { default as Avatar } from "./Avatar";
-export { default as Badge } from "./Badge";
